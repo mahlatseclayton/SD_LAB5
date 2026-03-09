9c2fe3f24b0e30965850926d3dd783be20372afc
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-03-09 15:15:46 +0200

    post

--- 2821750-lab5.js
+++ 2821750-lab5.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-let books=[
-    {id:"1",title:"macbethg"}
-];
+let books=[];
 app.use(express.json());
 
 // Your routes here
-app.get('/whoami/:studentNumber',(req,res)=>{
+app.get('/whoami',(req,res)=>{
     // Returns your student number
-    const studentNum=req.params.studentNumber;
+    const studentNum={studentNumber:"2821750"};
     res.json(studentNum);
 });
 app.get('/books',(req,res)=>{
     // return all books
     res.json(books);
+});
+//  get a specific book
+app.get('/books/:id',(req,res)=>{
+    const id=req.params.id;
+    const specific_book=books.find((book)=>book.id===id);
+   
+  if (!specific_book) {
+    return res.status(404).json({ error: "Student not found" });
+  }
+  res.json(specific_book);
+   
+});
+//  add a book in the books collection
+app.post('/books',(req,res)=>{
+    // get the book from body
+    const book={id:req.body.id,title:req.body.title,details:req.body.details};
+    if(!book){
+        res.status(404).json({error:"Missing required fields"});
+    }
+    res.json(book);
 });
 
 app.listen(PORT, () => {
